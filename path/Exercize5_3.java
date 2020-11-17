e61f6aeed26e20b1fac1a3eec051fdb71a115868
nosaka-y <67583194+nosaka-y@users.noreply.github.com>
2020-11-17 22:01:12 +0900

    Delete Exercize5_3.java

--- path/Exercize5_3.java
+++
@@ -1,9 +0,0 @@
-package prmn_lecture05;
-
-public class Exercize5_3 {
-    public static void main(String[] args) {
-        Butterfly fly = new Butterfly();
-        Insect insect = fly;
-        insect.move();
-    }
-}
